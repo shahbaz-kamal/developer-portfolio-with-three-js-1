905c16be5bd13f3d90472d8d18105b7131326e6b
shahbaz-kamal <shahbazkamal384@gmail.com>
2025-12-30 18:04:48 +0600

    started working

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
       <Button>Click me</Button>
+      <Button>Click me</Button>
     </div>
   )
 }
