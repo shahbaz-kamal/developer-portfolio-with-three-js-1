5acfc352f05d7d0af1be7ab5cb48fb5305df8215
shahbaz-kamal <shahbazkamal384@gmail.com>
2025-12-30 18:12:22 +0600

    completed home page

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ function App() {
     <div className="flex min-h-svh flex-col items-center justify-center">
       <Button>Click me</Button>
       <Button>Click me</Button>
+      <Button>Click me</Button>
     </div>
   )
 }
